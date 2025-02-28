cb6ae78bdc137a030c30dfef8148b04a10a12ef6
Yashx11 <ymali3387@gmail.com>
2025-02-28 23:09:38 +0530

    CI / CD Enabled
    Added the Dynamic browser switch also added the CI / CD git actions workflow to automatic run testcases

--- demoblaze/TestCase/conftest.py
+++ demoblaze/TestCase/conftest.py
@@ -1,10 +1,20 @@
 import pytest
 from selenium import webdriver
 from Utils.Logger import logger
+from Utils.readproperties import read_configurations
+
+browser = read_configurations("Basic Info", "browser")
 
 @pytest.fixture()
 def setup():
-    driver = webdriver.Chrome()
+    if browser.lower() == "chrome":
+        driver = webdriver.Chrome()
+    elif browser.lower() == "firefox":
+        driver = webdriver.Firefox()
+    elif browser.lower() == "edge":
+        driver = webdriver.Edge()
+    else:
+        return "Driver not Found"
     driver.maximize_window()
     return driver
 
